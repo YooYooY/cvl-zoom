56a3e2f39468edd0b2971335c5e8ff53be9d606e
chenwl <13751840799@163.com>
2024-05-22 19:18:08 +0800

    feat: init project

---
+++ constants/index.ts
@@ -0,0 +1,27 @@
+export const sidebarLinks = [
+  {
+    lable: 'Home',
+    route: '/home',
+    imgUrl: '/icons/home.svg',
+  },
+  {
+    lable: 'Upcoming',
+    route: '/upcoming',
+    imgUrl: '/icons/upcoming.svg',
+  },
+  {
+    lable: 'Previous',
+    route: '/previous',
+    imgUrl: '/icons/previous.svg',
+  },
+  {
+    lable: 'Recordings',
+    route: '/recordings',
+    imgUrl: '/icons/recordings.svg',
+  },
+  {
+    lable: 'Personal Room',
+    route: '/personal-room',
+    imgUrl: '/icons/add-personal.svg',
+  }
+]
---
+++ record.md
@@ -0,0 +1,12 @@
+### link
+
+https://www.youtube.com/watch?v=R8CIO1DZ2b8
+
+https://ui.shadcn.com/docs/installation/next
+
+
+https://www.figma.com/design/FSbYf8vm3wKN9cztXyt0sp/Zoom-Clone?node-id=1-22&t=xnOm0PovEnSq1Qy5-0
+
+
+
+https://github.com/adrianhajdin/zoom-clone
