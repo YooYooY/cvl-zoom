5ec7b01aeb247a1c8c349068d7434267d1b8b929
chenwl <13751840799@163.com>
2024-07-28 01:48:38 +0800

    chore:Meeting card

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,8 +1,11 @@
+import { SignIn } from '@clerk/nextjs'
 import React from 'react'
 
 const SignInPage = () => {
   return (
-    <main>SignInPage</main>
+    <main className='flex h-screen w-full items-center justify-center'>
+      <SignIn />
+    </main>
   )
 }
 
